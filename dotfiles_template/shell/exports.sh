1fdf36756756b5fb51296e2f85ec00d315427a99
Carlos Henry <chenryhabana205@gmail.com>
2020-12-11 23:59:57 -0500

    Update exports.sh

--- dotfiles_template/shell/exports.sh
+++ dotfiles_template/shell/exports.sh
@@ -23,7 +23,7 @@ export path=(
   "/usr/bin"
   "/usr/sbin"
   "/sbin"
-  "/Users/chenry/Java/android-sdk-macosx/platform-tools
+  "/Users/chenry/Java/android-sdk-macosx/platform-tools"
   "/Users/chenry/Java/flutter/bin"
   "/Users/chenry/Java/ngrok"
   "/Users/chenry/Java/terraform"
